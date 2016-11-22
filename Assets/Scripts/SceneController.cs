a666b43cfb447239f33770cdd7c1f9ba2efc0c24
xoakzxz <juandavid_ja@hotmail.com>
2016-11-21 21:50:14 -0500

    Update_21112016_Animations

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	[SerializeField]
+    private PlayerController playerController;
+
+    [HeaderAttribute("Animators")]
+    
+    [SerializeField]
+    private Animator playerAnimator;
+    [SerializeField]
+    private Animator fadeAnimator;
+
+    //Unity functions
+
+    void Start()
+    {
+        playerController.enabled = false;
+        fadeAnimator.SetTrigger("isFadeIn");
+        PlayIntroAnimation();
+    }
+
+    //Class functions
+
+    private void PlayIntroAnimation()
+    {
+        playerAnimator.SetTrigger("isIntro");
+    }
+
+    public void EnableController()
+    {
+        playerController.enabled = true;
+    }
+
+    private void Blink()
+    {
+        fadeAnimator.SetTrigger("isBlink");
+    }
+}
